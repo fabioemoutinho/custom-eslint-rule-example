9f9e0f4d830085b6395c82e2e023731d7b54fc9e
fabioemoutinho <fabioemoutinho@gmail.com>
2022-06-28 10:36:45 -0300

    host-listener-click-events-have-key-events rule

--- eslint/index.js
+++ eslint/index.js
@@ -1,3 +1,25 @@
+const hasHostListenerKeyEvent = function (members) {
+  const keyEvents = { keydown: 1, keypress: 1, keyup: 1 };
+
+  for (let i = 0; i < members.length; i++) {
+    const member = members[i];
+    if (member.type === "MethodDefinition" && member.kind === "method") {
+      const decorators = member.decorators;
+      for (let j = 0; j < decorators?.length; j++) {
+        const decorator = decorators[j];
+        const [event] = decorator.expression.arguments ?? [];
+        if (
+          decorator.expression.callee.name === "HostListener" &&
+          keyEvents.hasOwnProperty(event?.value)
+        ) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+};
+
 module.exports = {
   rules: {
     "my-awesome-rule": {
@@ -23,6 +45,40 @@ module.exports = {
                 data: {
                   filename,
                 },
+              });
+            }
+          },
+        };
+      },
+    },
+    "host-listener-click-events-have-key-events": {
+      meta: {
+        messages: {
+          hostListenerClickEventsHaveKeyEvents:
+            "HostListener click must be accompanied by either keyup, keydown or keypress event for accessibility.",
+        },
+      },
+      create: function (context) {
+        return {
+          Decorator: function (node) {
+            const expression = node.expression;
+            const isHostListener = expression.callee?.name === "HostListener";
+
+            if (!isHostListener) {
+              return;
+            }
+
+            const [event] = expression.arguments ?? [];
+            const isClickEvent = event?.value === "click";
+            if (!isClickEvent) {
+              return;
+            }
+
+            const hostClass = node.parent.parent;
+            if (!hasHostListenerKeyEvent(hostClass.body)) {
+              context.report({
+                node: node,
+                messageId: "hostListenerClickEventsHaveKeyEvents",
               });
             }
           },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'custom-eslint-rule-example';
+
+  @HostListener('click', ['$event'])
+  // @HostListener('keyup', ['$event'])
+  handleClick(event: MouseEvent) {
+    // TODO: do something
+  }
+
+  // @HostListener('keyup', ['$event'])
+  handleKeyUp(event: KeyboardEvent) {
+    // TODO: do something
+  }
 }
