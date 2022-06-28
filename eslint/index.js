c37dde62e5e528cdb6e2c03e012694add9404828
fabioemoutinho <fabioemoutinho@gmail.com>
2022-06-28 10:07:30 -0300

    no-testing rule

--- eslint/index.js
+++ eslint/index.js
@@ -6,5 +6,28 @@ module.exports = {
         return {};
       },
     },
+    "no-testing": {
+      meta: {
+        messages: {
+          noTesting: "Found '{{filename}}'. Sorry, no testing allowed.",
+        },
+      },
+      create: function (context) {
+        return {
+          Program: function (node) {
+            const filename = context.getFilename();
+            if (filename.includes(".spec")) {
+              context.report({
+                node,
+                messageId: "noTesting",
+                data: {
+                  filename,
+                },
+              });
+            }
+          },
+        };
+      },
+    },
   },
 };
